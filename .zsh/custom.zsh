dd99fba5819b6b04c6acbbbed97fa2f1ab37716a
Valentin Thomas <me@valentin-thomas.com>
2024-07-03 11:52:22 -0700

    Cleanup

--- .zsh/custom.zsh
+++ .zsh/custom.zsh
@@ -1,9 +1,7 @@
-plugins=(git wd vscode thefuck macos docker docker-compose)
+plugins=(git wd macos docker docker-compose)
 
 source $ZSH/oh-my-zsh.sh
 source ~/.zsh/env.zsh
-
-alias create-remix="npx create-remix@latest --package-manager yarn -y --template majortom327/lofi-stack --token $GH_TOKEN"
 
 alias ls='eza'
 alias la='ls -la'
--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -1,3 +1,2 @@
 export PATH=$HOME/bin:/usr/local/sbin:$PATH
-export PATH=$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH
 export PATH=$HOME/.cargo/bin:$PATH
